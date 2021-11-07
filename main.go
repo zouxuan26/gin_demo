250cbaf50644235162a22d17c4c41bb7080ba827
zouxuan <zouxuan@yunji.ai>
2021-11-07 23:57:55 +0800

    日常推送

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/hello", sayHello)
-	r.Run()
+func satHello(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("Parser template failed, err:%v", err)
+	}
+	name := "小王子"
+	err = t.Execute(w, name)
+	if err != nil {
+		fmt.Println("render template failed, err:%v", err)
+		return
+	}
 }
 
-func sayHello(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hello code",
-	})
+func main() {
+	http.HandleFunc("/", satHello)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("HTTP server start failed, err:%v", err)
+	}
 }
